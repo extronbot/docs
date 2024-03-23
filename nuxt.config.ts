919431634f7ece6ff5889d00ca46d1f53db34c05
Chris1238 <113909888+Chris1238@users.noreply.github.com>
2024-03-23 10:41:17 +0000

    Upload files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,75 +8,14 @@ export default defineNuxtConfig({
     }
   },
   speedkit: {
-    detection: {
-      performance: true,
-      browserSupport: true
-    },
-
-    performanceMetrics: {
-      timing: {
-        fcp: 800,
-        dcl: 1200
-      }
-    },
-
-    /* fonts: [{
-      family: "Font A",
-      locals: ["Font A"],
-      fallback: ["Arial", "sans-serif"],
-      variances: [
-        {
-          style: "normal",
-          weight: 400,
-          sources: [
-            { src: "@/assets/fonts/font-a-regular.woff", type:"woff" },
-            { src: "@/assets/fonts/font-a-regular.woff2", type:"woff2" }
-          ]
-        }, {
-          style: "italic",
-          weight: 400,
-          sources: [
-            { src: "@/assets/fonts/font-a-regularItalic.woff", type:"woff" },
-            { src: "@/assets/fonts/font-a-regularItalic.woff2", type:"woff2" }
-          ]
-        }, {
-          style: "normal",
-          weight: 700,
-          sources: [
-            { src: "@/assets/fonts/font-a-700.woff", type:"woff" },
-            { src: "@/assets/fonts/font-a-700.woff2", type:"woff2" }
-          ]
-        }
-      ]
-    }], */
-
-    //targetFormats: ["webp", "avif", "jpg|jpeg|png|gif"],
-
-    componentAutoImport: false,
-    componentPrefix: undefined,
-
-    lazyOffset: {
-      component: "0%",
-      asset: "0%"
-    }
+    disableNuxtImage: true,
+    disableNuxtFontaine: true
   },
   image: {
-    screens: {
-      default: 320,
-      xxs: 480,
-      xs: 576,
-      sm: 768,
-      md: 996,
-      lg: 1200,
-      xl: 1367,
-      xxl: 1600,
-      "4k": 1921
-    },
-    format: ["avif", "webp"],
-    domains: ["img.youtube.com", "i.vimeocdn.com"],
+    format: ["avif", "webp", "jpg|jpeg|png|gif"],
+    /* domains: ["cdn.discord.com"],
     alias: {
-      youtube: "https://img.youtube.com",
-      vimeo: "https://i.vimeocdn.com",
-    }
+      discord: "https://cdn.discord.com"
+    } */
   }
 })
